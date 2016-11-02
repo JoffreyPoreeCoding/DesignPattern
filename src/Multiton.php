574c4d7116f276431ddd3b775173cd5e21bc21ab
joffrey <joffrey.p@compilatio.net>
2016-11-02 14:52:00 +0100

    Add __wakeup function

--- src/Multiton.php
+++ src/Multiton.php
@@ -3,32 +3,37 @@
 namespace JPC\DesignPattern;
 
 trait Multiton {
+
     private static $instance = [];
 
     public static function getInstance($identifier, ...$params) {
         $class = get_called_class();
-        
+
         if (!isset(static::$instance[$identifier])) {
             $reflection = new \ReflectionClass($class);
             $constructor = $reflection->getConstructor();
-            
+
             $object = $reflection->newInstanceWithoutConstructor();
-            
+
             if ($constructor != null) {
                 $constructor->invokeArgs($object, $params);
             }
-                    
+
             static::$instance[$identifier] = $object;
         }
-        
+
         return static::$instance[$identifier];
     }
-    
-    protected static function setInstance($identifier, $instance){
+
+    protected static function setInstance($identifier, $instance) {
         static::$instance[$identifier] = $instance;
     }
-    
-    public function __clone(){
+
+    public function __clone() {
         throw new Exception\MultitonException("Unable to clone a multiton object");
+    }
+
+    public function __wakeup() {
+        throw new Exception\MultitonException("Unable to unserialize a multiton object");
     }
 }
--- src/Singleton.php
+++ src/Singleton.php
@@ -3,32 +3,42 @@
 namespace JPC\DesignPattern;
 
 trait Singleton {
+
     private static $instance;
 
     public static function getInstance(...$params) {
         $class = get_called_class();
-        
+
         if (!isset(static::$instance)) {
             $reflection = new \ReflectionClass($class);
             $constructor = $reflection->getConstructor();
-            
+
             $object = $reflection->newInstanceWithoutConstructor();
-            
+
             if ($constructor != null) {
                 $constructor->invokeArgs($object, $params);
             }
-                    
+
             static::$instance = $object;
         }
-        
+
         return static::$instance;
     }
-    
-    protected static function setInstance($instance){
+
+    protected static function setInstance($instance) {
         static::$instance = $instance;
     }
-    
-    public function __clone(){
+
+    public function __clone() {
         throw new Exception\SingletonException("Unable to clone a singleton object");
     }
+
+    public function __wakeup() {
+        if ($this->getInstance() != null) {
+            throw new Exception\SingletonException("Unable to unserialize a singleton object");
+        } else {
+            static::setInstance($this);
+        }
+    }
+
 }
